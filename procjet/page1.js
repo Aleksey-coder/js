8a60a4b7844bf562810bcdee83fec0a20065659b
Aleksey Prokopiev <1641387@mail.ru>
2021-03-29 01:55:41 +0800

    Update page1.js

--- procjet/page1.js
+++ procjet/page1.js
@@ -1,5 +1,5 @@
-var drawCats = function (howManyTimes) {
-  for (var i = 0; i < howManyTimes; i++) {
+let drawCats = function (howManyTimes) {
+  for (let i = 0; i < howManyTimes; i++) {
     console.log(i + " =^.^=");
   }
 };
