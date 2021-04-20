a50bd63dbfcc586171d9ac249b8d1a1da3232d93
Aleksey Prokopiev <1641387@mail.ru>
2021-04-21 00:35:31 +0800

    Update page16.js

--- procjet/page16.js
+++ procjet/page16.js
@@ -1,5 +1,5 @@
-var dog = { name: "Pancake", age: 6, color: "white", bark: "Yip yap yip!" };
-var cat = { name: "Harmony", age: 8, color: "tortoiseshell" };
+let dog = { name: "Pancake", age: 6, color: "white", bark: "Yip yap yip!" };
+let cat = { name: "Harmony", age: 8, color: "tortoiseshell" };
 Object.keys(dog);
 // ["name", "age", "color", "bark"]
 Object.keys(cat);
