8cb2bf848311d017bd68e012018c5573895bf665
Aleksey Prokopiev <1641387@mail.ru>
2021-05-21 19:59:25 +0800

    Add files via upload

---
+++ procjet/page25.js
@@ -0,0 +1,31 @@
+let num = 50;
+
+if (num < 49) {
+	console.log('Неверно!')
+} else if (num > 100) {
+	console.log('Много!')
+} else { 
+	console.log('Верно')
+};
+
+(num == 50) ? console.log('Верно') : console.log('Неверно!');
+	
+	
+switch (num) {
+	case num < 49:
+		console.l:Неверно!');
+		break;   
+	case num > 100:
+		console.log('Много!');
+		break;
+	case num > 80:
+		console.log('Все еще много!');
+		break;
+	case 50:
+		console.log('Верно');
+		break;
+	default:
+		console.log('Что-то пошло не так!');
+		break;
+}
+
