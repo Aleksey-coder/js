39eebc33db23974ec95b6ba9cef124379c81ca4c
Aleksey Prokopiev <1641387@mail.ru>
2021-06-05 00:04:34 +0800

    Add files via upload

---
+++ procjet/page35.js
@@ -0,0 +1,11 @@
+let str = "test";
+console.log(str.length);
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+let twelve = "12.2px";
+
+//console.log(Math.round(twelve));
+console.log(parseInt(twelve));
+console.log(parseFloat(twelve));
