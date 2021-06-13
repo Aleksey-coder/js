aa30b01e355d3a984a0e087f5842b16f93fb1f38
Aleksey Prokopiev <1641387@mail.ru>
2021-06-14 01:22:37 +0800

    Add files via upload

---
+++ procjet/page43.js
@@ -0,0 +1,13 @@
+let arr = [1, 2, 3, 4, 5];
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+//
+
+let arr = [1, 2, 3, 4, 5];
+arr[99] = 99;
+console.log(arr.length);
+console.log(arr);
+
+
