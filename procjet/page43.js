94adc31a964d0e88324de61c1d7247a910b06243
Aleksey Prokopiev <1641387@mail.ru>
2021-06-17 02:10:41 +0800

    Update page43.js

--- procjet/page43.js
+++ procjet/page43.js
@@ -3,11 +3,23 @@ for (let i = 0; i < arr.length; i++) {
     console.log(arr[i]);
 }
 
-//
+// 1
+// 2
+// 3
+// 4
+// 5
 
 let arr = [1, 2, 3, 4, 5];
 arr[99] = 99;
 console.log(arr.length);
 console.log(arr);
 
-
+100
+(100) [1, 2, 3, 4, 5, empty × 94, 99]
+0: 1
+1: 2
+2: 3
+3: 4
+4: 5
+99: 99
+length: 100
