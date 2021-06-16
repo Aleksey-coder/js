7d78fe56a58e70196136510419544eaf5fa78fe1
Aleksey Prokopiev <1641387@mail.ru>
2021-06-16 18:02:27 +0800

    Update page44.js

--- procjet/page44.js
+++ procjet/page44.js
@@ -7,6 +7,17 @@ arr.forEach(function(item, i, mass) {
 console.log(arr);
 
 
-
+//0: first(массив: first,2,3,four,5)
+//1: 2(массив: first,2,3,four,5)
+//2: 3(массив: first,2,3,four,5)
+// 3: four(массив: first,2,3,four,5)
+// 4: 5(массив: first,2,3,four,5)
+// (5) ["first", 2, 3, "four", 5]
+// 0: "first"
+// 1: 2
+// 2: 3
+// 3: "four"
+// 4: 5
+// length: 5
 
 
