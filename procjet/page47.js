45630c4f4ba7404cf40d84d77a8b98f93c259318
Aleksey Prokopiev <1641387@mail.ru>
2021-06-24 01:14:45 +0800

    Add files via upload

---
+++ procjet/page47.js
@@ -0,0 +1,13 @@
+let arr = ["aawe", 'zzz', 'pp', 'rga'],
+    i = arr.join(', ');
+    console.log(arr);
+
+    //[ 'aawe', 'zzz', 'pp', 'rga' ]
+
+let arr = ["aawe", 'zzz', 'pp', 'rga'],
+    i = arr.join(', ');
+    console.log(i);
+
+    //aawe, zzz, pp, rga
+
+    
