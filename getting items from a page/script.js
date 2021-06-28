d6e0d48f75fbde6f44753352e04e888689b8e0a8
Aleksey Prokopiev <1641387@mail.ru>
2021-06-29 00:30:19 +0800

    Add files via upload

---
+++ getting items from a page/script.js
@@ -0,0 +1,10 @@
+let box = document.getElementById('box'),
+    btn = document.getElementsByName('button'),
+    circle = document.getElementsByClassName('circle');
+    heart = document.querySelectorAll('.heart');
+
+    console.log(box);
+    console.log(btn);
+    console.log(btn[0]);
+    console.log(circle[2]);
+    console.log(heart);
