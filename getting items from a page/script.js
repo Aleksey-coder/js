8b7b601b3632b5d6c3b783e9a50d02d34b9cfb6d
Aleksey Prokopiev <1641387@mail.ru>
2021-06-30 01:10:05 +0800

    Update script.js

--- getting items from a page/script.js
+++ getting items from a page/script.js
@@ -4,9 +4,4 @@ let box = document.getElementById('box'),
     heart = document.querySelectorAll('.wrapper.heart'),
     oneHeart = document.querySelectorAll('.heart');
 
-    console.log(box);
-    console.log(btn);
-    console.log(btn[0]);
-    console.log(circle[2]);
-    console.log(heart[1]);
-    console.log(oneHeart);
+    
