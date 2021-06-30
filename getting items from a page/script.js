8c7622b79fe2d5e07c8f2f3a1a3a243bffdce686
Aleksey Prokopiev <1641387@mail.ru>
2021-07-01 00:35:03 +0800

    Update script.js

--- getting items from a page/script.js
+++ getting items from a page/script.js
@@ -5,6 +5,10 @@ let box = document.getElementById('box'),
     oneHeart = document.querySelectorAll('.heart');
 
     
-for (let i = 0; i < heart.length; i++) {
-    heart[i].style.backgroundColor = 'blue';
-}
+// for (let i = 0; i < heart.length; i++) {
+//     heart[i].style.backgroundColor = 'blue';
+// }
+
+heart.forEach(function(item, i, hearts) {
+    item.style.backgroundColor = 'blue';
+});
