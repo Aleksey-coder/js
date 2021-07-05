2c173db34dd7f684c70fe4c2d5d4dcfc67ed4232
Aleksey Prokopiev <1641387@mail.ru>
2021-07-06 00:19:50 +0800

    Update style.js

--- Event handler/style.js
+++ Event handler/style.js
@@ -4,3 +4,10 @@ btn[0].onclick = function() {
     alert('Вы нажили первую кнопку!');
     alert('Вы опять нажили первую кнопку!');
 }
+
+btn[0].addEventListener('click', function(event) {
+    console.log(event);
+    let target = event.target;
+    target.style.display = 'none';
+    console.log('Произошло событие: ' + event.type +  ' на элементе ' + event.target);
+});
