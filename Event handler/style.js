f06dba09fbeb582680fcd7443f1dc878f0d0c79b
Aleksey Prokopiev <1641387@mail.ru>
2021-07-11 00:27:47 +0800

    Update style.js

--- Event handler/style.js
+++ Event handler/style.js
@@ -21,6 +21,12 @@ link.addEventListener('click', function(event) {
     console.log('Произошло событие: ' + event.type +  ' на элементе ' + event.target);
 });
 
+btn.forEach(function(item) {
+    item.addEventListener('mouseleave', function() {
+        console.log("Вышли!");
+    })
+});
+
 
 // btn[0].addEventListener('mouseenter', function() {
 //     alert('Вы навели на первую кнопку!');
