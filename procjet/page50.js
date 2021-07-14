c4219c72acce75b7b9be877f4215c2099666d784
Aleksey Prokopiev <1641387@mail.ru>
2021-07-15 00:40:46 +0800

    Update page50.js

--- procjet/page50.js
+++ procjet/page50.js
@@ -1,6 +1,12 @@
-let timerId = setTimeout(sayHello, 3000);
-clearTimeout(timerId);
+// let timerId = setTimeout(sayHello, 3000);
+// clearTimeout(timerId);
 
+// function sayHello() {
+// 	alert('Hello World!');
+// };
+
+let timerId = setInterval(sayHello, 3000);
+//clearTimeout(timerId);
 function sayHello() {
 	alert('Hello World!');
 };
