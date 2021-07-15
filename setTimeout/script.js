3904ee306d90759b74adae04d623110455d43d11
Aleksey Prokopiev <1641387@mail.ru>
2021-07-16 00:07:07 +0800

    Add files via upload

---
+++ setTimeout/script.js
@@ -0,0 +1,28 @@
+let btn = document.querySelector('.btn'),
+    elem = document.querySelector('.box');
+
+function myAnimation() {
+    let pos = 0;
+
+    let id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 300) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+}
+
+btn.addEventListener('click', myAnimation);
+
+let btnBlock = document.querySelector('.btn-block'),
+    btns = document.getElementsByTagName('button');
+
+btnBlock.addEventListener('click', function(event) {
+    if (event.target && event.target.matches('button.first')) {
+        console.log("Hello!");
+    }
+});
