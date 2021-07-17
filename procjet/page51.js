66520b02f2738182aca95e9b065e522700c3cce4
Aleksey Prokopiev <1641387@mail.ru>
2021-07-18 01:53:46 +0800

    Add files via upload

---
+++ procjet/page51.js
@@ -0,0 +1,43 @@
+//es5 
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function() {
+//         console.log('Hello! ' + this.name);
+//     }
+// }
+// User.prototype.exit = function(name) {
+//     console.log('Пользователь ' + this.name + ' ушел');
+// }
+
+// let ivan = new User('Ivan', 52);
+//     alex = new User('Aleksey', 30);
+
+// console.log(ivan);
+// console.log(alex);
+
+// ivan.exit();
+
+//es6 новый метод 
+
+class User {
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
+        this.human = true;  
+    }
+    hello() {
+        console.log(`Hello! ${this.name}`)
+    }
+    exit() {
+        console.log(`Пользователь ${this.name} ушел`)
+    }
+}
+
+let ivan = new User('Ivan', 52);
+let alex = new User('Aleksey', 30);
+console.log(ivan);
+console.log(alex);
+ivan.hello();
+alex.hello();
