5b1eab7d83d450ec4f634b016791834a9515943d
Aleksey Prokopiev <1641387@mail.ru>
2021-07-21 01:21:27 +0800

    Add files via upload

---
+++ procjet/page54.js
@@ -0,0 +1,22 @@
+let user = {
+	name: "Jonh"
+};
+
+function sayName(surname) {
+	console.log(this);
+	console.log(this.name + surname);
+}
+
+console.log(sayName.call(user, 'Smith'));
+console.log(sayName.apply(user, ['Snow']));
+
+function count(number) {
+	return this * number;
+}
+
+let double = count.bind(2);
+console.log(double(3));
+console.log(double(10));
+
+
+// 4 Указывает конкретного контекста - call, apply, bind
