f98534f0ff90ce8e3cc9cc105b2cde9950631bf6
Aleksey Prokopiev <1641387@mail.ru>
2021-08-05 00:08:52 +0800

    Add files via upload

---
+++ es6.Promise/yesPromise.js
@@ -0,0 +1,35 @@
+//Promise
+let drink = 1;
+
+function shoot(arrow) {
+    console.log('Вы сделали выстрел...');
+    let promise = new Promise(function(resolve, reject) {
+        setTimeout(function(){
+            Math.random() > .5 ? resolve({}) : reject("Вы промахнулись");
+        }, 3000);
+    }); 
+    return promise;
+};
+
+
+function win(){
+    console.log('Вы победили!');
+    (drink == 1) ? buyBeer() : giveMoney();
+}
+
+function buyBeer(){
+    console.log('Вам купили пиво!')
+}
+
+function giveMoney(){
+    console.log('Вам заплатили!')
+}
+
+function loose(){
+    console.log('Вы проиграли!')
+}
+
+shoot({})
+        .then(mark => console.log('Вы попали!'))
+        .then(win)
+        .catch(loose)
