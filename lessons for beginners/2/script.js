98f9fea7920eed1c5649f37255bc0f0f624484d5
Aleksey Prokopiev <1641387@mail.ru>
2021-09-30 00:11:08 +0800

    Update script.js

--- lessons for beginners/2/script.js
+++ lessons for beginners/2/script.js
@@ -3,6 +3,10 @@ const  handleClick = (event) => {
     console.log(event)
 }
 
+window.addEventListener('click', function() {
+   console.log('Window Click!')
+})
+
 buttons.forEach(button => {
     button.addEventListener('click', handleClick) 
     })
