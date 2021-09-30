375221a2a5bb54a7407ee761bb1b8667cbbd9e37
Aleksey Prokopiev <1641387@mail.ru>
2021-10-01 01:00:06 +0800

    Update script.js

--- lessons for beginners/2/script.js
+++ lessons for beginners/2/script.js
@@ -1,12 +1,22 @@
 const buttons = document.querySelectorAll('.btn');
 const  handleClick = (event) => {
-    console.log(event)
+   console.log("target >", event.target)
+   console.log("curtarget >", event.currentTarget)
+   console.log(event.target === event.currentTarget)
+    //event.stopPropagation()
 }
 
-window.addEventListener('click', function() {
-   console.log('Window Click!')
-})
+window.addEventListener('click', function(event) {
+   console.log('Window Click!', event.target)
+    event.stopPropagation()
+}, {capture: true})
 
 buttons.forEach(button => {
     button.addEventListener('click', handleClick) 
-    })
+})
+
+const img = document.querySelector('img');
+img.addEventListener('mouseover', (event) => {
+    console.log(event.currentTarget)
+    console.log(this)
+})
