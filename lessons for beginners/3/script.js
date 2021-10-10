44bd1f367f9a5da29f864650db1d102671bedc69
Aleksey Prokopiev <1641387@mail.ru>
2021-10-11 01:13:51 +0800

    Update script.js

--- lessons for beginners/3/script.js
+++ lessons for beginners/3/script.js
@@ -1,1 +1,14 @@
-const buttons = document.querySelectorAll('.btn');
+var years
+console.log(years)
+years = 100;
+
+letsGo()
+
+function letsGo() {
+	console.log('Go!!!')
+	console.log(add(5, 3))
+}
+
+function add(a, b) {
+	return a + b;
+}
