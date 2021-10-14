42a81f07889b4e4f46e61d5c667163a9785f24bd
Aleksey Prokopiev <1641387@mail.ru>
2021-10-15 01:18:13 +0800

    Update script.js

--- lessons for beginners/4/script.js
+++ lessons for beginners/4/script.js
@@ -1,3 +1,12 @@
 function external() {
-	const externalVar = 'Я - внешняя функция';
+	const externalVar = 'Я - внешняя функция'; 
+	
+  function internal() {
+	  const internalVar = 'Я - внутренняя функция';
+	  console.log('internalVar >', internalVar)
+	  console.log('externalVar >', externalVar)
+  }	
+   internal();
 }
+
+external();
