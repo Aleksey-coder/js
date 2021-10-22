1c8ed7d93dfae8e1fa5e7747016b8fd8b359207c
Aleksey Prokopiev <1641387@mail.ru>
2021-10-23 00:41:30 +0800

    Update script.js

--- lessons for beginners/5/script.js
+++ lessons for beginners/5/script.js
@@ -3,3 +3,23 @@ div.classList.add('wrapper');
 const body = document.querySelector('body')
 console.log(body)
 body.appendChild(div)
+const header = document.createElement('h1');
+header.textContent = 'все было всегда';
+div.insertAdjacentElement('beforebegin', header)
+
+const ul = `
+	<ul>
+		 <li>один</li>
+		 <li>два</li>
+		 <li>три</li>
+	</ul>
+`;
+div.innerHTML = ul;
+const img = document.createElement('img');
+img.src = 'https://picsum.photos/240';
+img.width = 240;
+img.classList.add('super');
+img.alt = 'Super Man';
+div.appendChild(img)
+console.log(img)	
+
