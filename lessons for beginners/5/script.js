647dbc8495d448550183c6510a4b6df3a138eb26
Aleksey Prokopiev <1641387@mail.ru>
2021-10-26 01:11:10 +0800

    Update script.js

--- lessons for beginners/5/script.js
+++ lessons for beginners/5/script.js
@@ -31,4 +31,41 @@ ulList.insertAdjacentHTML('beforebegin', elemHTML)
 const pDiv = document.querySelector('.pDiv');
 pDiv.children[1].classList.add('text');
 pDiv.firstElementChild.remove();
+const generateAutoCard = (brand, color, year) => {
+	const curDate = new Date();
+	const curYear = curDate.getFullYear();
+	return `
+		<div class = "autoCard">
+			<div>
+				<h2>${brand.toUpperCase()} ${year}</h2>
+				<p>Авомобиль ${brand.toUpperCase()} - ${year} года. Возраст авто - ${curYear - year} лет.</p>
+				<p>Цвет: ${color}</p>
+				<Button type='button' class='btn'>Удолить</button>
+			</div>	
+		</div>
+		`;
+}
+const carsDiv = document.createElement('div');
+carsDiv.classList.add('autos');
+
+const carsList = [
+	{brand: 'Tesla', year: 2015, color: 'red'},
+	{brand: 'Lexus', year: 2016, color: 'silver'},
+	{brand: 'Nissan', year: 2012, color: 'black'},
+]
+
+const carsHTML = carsList.map(car => {
+    return generateAutoCard(car.brand, car.color, car.year);
+}).join('');
+carsDiv.innerHTML = carsHTML;
+div.insertAdjacentElement('beforebegin', carsDiv)
+const buttons = document.querySelectorAll('.btn');
+function handleClick(e) {
+	const currentButton = e.currentTarget;
+	currentButton.closest('.autoCard').remove();
+	//console.log(currentButton.parentElement)
+}
+buttons.forEach(button => {
+	button.addEventListener('click', handleClick)
+})
 
