6e60a74d030ca2de69be91e96e3e4d244f6cb671
Aleksey Prokopiev <1641387@mail.ru>
2021-10-29 00:25:27 +0800

    Add files via upload

---
+++ lessons for beginners/6/script.js
@@ -0,0 +1,15 @@
+let name = 'larisa';
+let nameTwo = name;
+name = 'alex';
+
+console.log(name)
+console.log(nameTwo)
+
+let age = 100;
+let ageTwo = 15;
+age = 25;
+
+console.log(age)
+console.log(ageTwo)
+
+const students = ['Сергей', 'Станислав', 'Мария', 'Павел',];
