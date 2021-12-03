e24ca49cc4c8055ffb9490804d7011da48dcf1d8
Aleksey Prokopiev <1641387@mail.ru>
2021-12-04 00:13:48 +0800

    Update script.js

--- basics of programming/dom-page output/script.js
+++ basics of programming/dom-page output/script.js
@@ -4,3 +4,8 @@ document.querySelector('p').innerText += ' <b>my</b> friend';
 document.querySelector('h1').outerHTML = '<h2>replaced the whole h1 element</h2>'
 document.querySelector('h2').outerText = '<h2>replaced the whole h1 element</h2>'
 //---------------
+document.querySelector('div').insertAdjacentHTML('beforebegin', 1111);//до самого element (до открывающего тега)
+document.querySelector('div').insertAdjacentHTML('afterbegin', 2222);//сразу после открывающего тега  element (перед первым потомком).
+document.querySelector('div').insertAdjacentHTML('afterend', 55555);// после element (после закрывающего тега).
+document.querySelector('div').insertAdjacentHTML('beforeend', 44444);// сразу перед закрывающим тегом element (после последнего потомка).
+//---------------
