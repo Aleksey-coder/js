f2f7f439bfc33633450c220f1c9c1791def885dd
Aleksey Prokopiev <1641387@mail.ru>
2021-12-27 00:21:50 +0800

    Update script.js

--- Function/Функции - введение/script.js
+++ Function/Функции - введение/script.js
@@ -8,5 +8,15 @@ function hello() {
 	console.log('Hello');
 }
 
+function testFunction2() {
+	function t3() {
+		console.log('t3 function')
+	}
+	t3()
+	console.log('testFunction 2');
+}
+
 hello();
 testFunction();
+testFunction2();
+//t3(); !Error
