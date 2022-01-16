ea9a28fdd87211d50f715bc1ca3b966e3790ad20
Aleksey Prokopiev <1641387@mail.ru>
2022-01-17 00:43:12 +0800

    Update script.js

--- javascript_for_coders/accordion/script.js
+++ javascript_for_coders/accordion/script.js
@@ -1,2 +1,9 @@
 const headers = document.querySelectorAll("[data-name='accordeon-title']");
 	console.log("headers", headers);
+	headers.forEach(function(item){
+		item.addEventListener("click", toggleBlock)
+	});
+	
+	function toggleBlock() {
+		console.log("Fired");
+	}
