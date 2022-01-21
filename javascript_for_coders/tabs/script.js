a236d3007f5ebb4ca0f8b8669be401160d468e70
Aleksey Prokopiev <1641387@mail.ru>
2022-01-22 00:10:51 +0800

    Update script.js

--- javascript_for_coders/tabs/script.js
+++ javascript_for_coders/tabs/script.js
@@ -1,7 +1,11 @@
+//Нашли все заголовки по data атребуту
 const tabHeaders = document.querySelectorAll("[data-tab]");
+// Для каждого заголовка запустили ф-ю tabsHandler
 tabsHeaders.forEach(tabHandler);
 
 function tabHandler(item) {
 	console.log(item);
+	console.log(index);
+	// Находим название ID связонного блока с контентом
 	item.addEventListener("click", tabsClick)
 }
