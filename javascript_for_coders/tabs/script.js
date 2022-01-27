7782ffe3bca3dc0be499c1bf5218f7ce44d4b934
Aleksey Prokopiev <1641387@mail.ru>
2022-01-28 00:35:40 +0800

    Update script.js

--- javascript_for_coders/tabs/script.js
+++ javascript_for_coders/tabs/script.js
@@ -1,12 +1,13 @@
 //Нашли все заголовки по data атребуту
-const tabHeaders = document.querySelectorAll("[data-tab]");
+const tabsHeaders = document.querySelectorAll("[data-tab]");
+
 // Для каждого заголовка запустили ф-ю tabsHandler
 tabsHeaders.forEach(tabHandler);
 
-function tabHandler(item) {
+function tabHandler(item, index) {
 	console.log(item);
 	console.log(index);
-	// Находим название ID связонного блока с контентом
+	// Для каждего заголовка по клику запускаем ф-ю tabsClick
 	item.addEventListener("click", tabsClick)
 }
 
@@ -15,13 +16,16 @@ function tabsClick() {
 	console.dir(this.dataset.tab);
 	// Находим название ID связонного блока с контентом
 	const tabId = this.dataset.tab;
-//Скрываем все блоки с контентом
-//Находим по data атрибуту все блоки с контентом
-//и скрываем их
-
-document.querySelectorAll("[data-tab-content]").forEach(function(item) {
+	
+	//Скрываем все блоки с контентом
+	//Находим по data атрибуту все блоки с контентом
+	//и скрываем их
+	document.querySelectorAll("[data-tab-content]").forEach(function(item) {
 		item.classList.add("tab-content-hidden");
-});
+	});
+	
+	// По ID включаем нужный блок с контентом
+	//document.getElementById(tabId).classList.toggle("tab-content-hidden");
+	document.getElementById(tabId).classList.remove("tab-content-hidden");
+}
 
-// По ID включаем нужный блок с контентом
-//document.getElementById(tabId).classList.toggle("tab-content-hidden");
