bdff8ae02a26ae45b8cda703e9e5e5baacada8df
Aleksey Prokopiev <1641387@mail.ru>
2022-02-23 00:05:23 +0800

    Update script.js

--- D_T/Несколько обработчиков одного события в JavaScript/script.js
+++ D_T/Несколько обработчиков одного события в JavaScript/script.js
@@ -10,15 +10,10 @@ function func3() {
 	alert('3');
 }
 
-function func3() {
-	alert('4');
-}
-
 
 let button = document.querySelector('#button');
 
 button.addEventListener('click', func1);
 button.addEventListener('click', func2);
 button.addEventListener('click', func3);
-button.addEventListener('click', func4);
 
