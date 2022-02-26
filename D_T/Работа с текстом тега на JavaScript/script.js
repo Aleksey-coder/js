1433343fcb1b4c615bcc8d5b60e408d128a30b4a
Aleksey Prokopiev <1641387@mail.ru>
2022-02-27 00:58:56 +0800

    Update script.js

--- D_T/Работа с текстом тега на JavaScript/script.js
+++ D_T/Работа с текстом тега на JavaScript/script.js
@@ -16,4 +16,24 @@ btn.addEventListener('click', function() {
 	alert(sum2 = 3 * 3);
 });
 
+//Задача 2
+
+let btnK = document.querySelector('#btnK');
+let btnG = document.querySelector('#btnG');
+let str1 = document.querySelector('#str1');
+let str2 = document.querySelector('#str2');
+
+btnK.addEventListener('click', function() {
+	alert(str1.innerHTML); 
+});
+
+btnG.addEventListener('click', function() {
+	alert(str2.innerHTML);
+});
+
+//----
+
+button.addEventListener('click', function() {
+	elem.innerHTML = '!!!';
+});
 
