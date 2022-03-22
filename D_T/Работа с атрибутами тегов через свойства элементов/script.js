25de89a1d66d79947ee4b23d59f2dafa8feec214
Aleksey Prokopiev <1641387@mail.ru>
2022-03-23 01:35:14 +0800

    Add files via upload

---
+++ D_T/Работа с атрибутами тегов через свойства элементов/script.js
@@ -0,0 +1,31 @@
+let button = document.querySelector('#button');
+let elem = document.querySelector('#elem');
+
+button.addEventListener('click', function() {
+	alert(elem.id);
+	alert(elem.type);
+});
+
+button.addEventListener('click', function() {
+	elem.type = 'submit';
+});
+
+//Задача 1
+
+let btn1 = document.querySelector('#btn1');
+let em1 = document.querySelector('#em');
+
+btn1.addEventListener('click', function() {
+	alert(em1.id);
+	alert(em1.type);
+});
+
+btn1.addEventListener('click', function() {
+	em1.type = 'email';
+})
+
+//Задача 2
+
+button.addEventListener('click', function() {
+	elem.type = 'submit'; // присвоим новое значение атрибуту type
+});
