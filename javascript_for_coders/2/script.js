f2b190956bd37e5b767f93e3234f3e6d8c05a478
Aleksei Prokopev <1641387@mail.ru>
2022-06-07 00:37:53 +0800

    Update script.js

--- javascript_for_coders/2/script.js
+++ javascript_for_coders/2/script.js
@@ -1,17 +1,14 @@
-window.onload=function()
-{
+window.onload=function() {
 document.getElementById("lin").onclick=look;
 document.getElementById("view").onclick=del;
 };
 
-function look()
-{
+function look() {
 document.getElementById("view").style.visibility="visible"; 
 document.getElementById("bas").style.visibility="visible"; return false;
 }
 
-function del()
-{
+function del() {
 document.getElementById("view").style.visibility="hidden"; 
 document.getElementById("bas").style.visibility="hidden";
 }
