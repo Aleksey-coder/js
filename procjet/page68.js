c669ff685499dfbe440f2421bf695551f24aa135
Aleksei Prokopev <1641387@mail.ru>
2022-06-17 00:10:08 +0800

    Update page68.js

--- procjet/page68.js
+++ procjet/page68.js
@@ -1,6 +1,6 @@
 let strSum = prompt("Введите сумму вклада", 1000);
 let strPercent = prompt("Введите процентную ставку", 10);
-var sum = parseInt(strSum);
+let sum = parseInt(strSum);
 var procent = parseInt(strPercent);
     sum = sum + sum * procent / 100;
     alert("После начисления процентов сумма вклада составит: " + sum);
