19e9b7c54f062c91083a96c6cf48f147a6e19684
Aleksei Prokopev <1641387@mail.ru>
2022-07-31 00:16:58 +0800

    Update script.js

--- javascript_for_coders/1/script.js
+++ javascript_for_coders/1/script.js
@@ -1,6 +1,7 @@
 window.onload=function(){
 document.getElementById("lin").onclick=function(){
-window.open("picture.jpg", " ", "width=500, height=500"); 
+//window.open("picture.jpg", " ", "width=500, height=500"); 
+  window.open("picture.jpg", " ", "width=100%");
 return false;
 };
 };
